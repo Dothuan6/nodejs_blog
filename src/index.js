48e491dfc1ca2c0735532f02aeb6862067a7173d
Dothuan6 <thuann6222@gmail.com>
2024-05-31 22:15:58 +0700

    test searching

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 
 //file tĩnh
 app.use(express.static(path.join(__dirname, "public")));
+//middleware xử lý dữ liệu từ  form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // http logger
 app.use(morgan("dev"));
 //template engine
@@ -18,7 +22,16 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/news", (req, res) => {
+  console.log(req.query.q);
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body.q);
+  res.render("search");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
