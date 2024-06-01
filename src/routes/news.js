599d32065362fc5f05ec42d5f0dfd543b1c5c192
Dothuan6 <thuann6222@gmail.com>
2024-06-01 17:37:47 +0700

    install prettier husyky lint-staged

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const newsController = require("../app/controllers/NewsController");
+const newsController = require('../app/controllers/NewsController');
 
-router.use("/:slug", newsController.show);
+router.use('/:slug', newsController.show);
 
-router.use("/", newsController.index);
+router.use('/', newsController.index);
 
 module.exports = router;
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const siteController = require("../app/controllers/SiteController");
+const siteController = require('../app/controllers/SiteController');
 
-router.use("/search", siteController.search);
+router.use('/search', siteController.search);
 
-router.use("/", siteController.index);
+router.use('/', siteController.index);
 module.exports = router;
